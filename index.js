ca4a8ab1bd53aad847f403d7fb06d4d70ec0dacc
aixion <aixion1506@gmail.com>
2024-03-11 21:53:53 +0900

    오류처리 미들웨어 구현

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ const setUser = require('./middlewares/set-user');
 
 const app = express();
 
+const hasError = (req, res, next) => {
+    next("ERROR");
+}
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('START');
 });
+
+app.get('/success', (req, res) => {
+    res.send("SUCCESS");
+});
+
+app.get('/fail', hasError, (req, res) => {
+    res.send("FAIL");
+})
 
 app.use('/notes', auth, noteRouter);
 app.use('/authors', authorRouter);
@@ -34,6 +46,11 @@ app.use((req, res, next) => {
         error : `Page not found ${req.path}`,
     });
 });
+
+// 오류처리 미들웨어 추가
+app.use((err, req, res, next) => {
+    res.send(`Request failed with ${err}`);
+})
 
 const PORT = 4000;
 app.listen(PORT, () => {
